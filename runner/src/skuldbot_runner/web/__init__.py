a6d6467cd1c64a967032d14aa7fc13bee68245a9
Dubiel Valdivia <d.valdivia@skuldbot.com>
2026-04-11 15:19:59 -0400

    refactor(runner): consolidate runner app and remove embedded web ui

--- runner/src/skuldbot_runner/web/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Local web UI for Runner configuration and monitoring."""
-
-from .app import create_app, start_web_server, RunnerState
-
-__all__ = ["create_app", "start_web_server", "RunnerState"]
